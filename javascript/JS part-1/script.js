6c4e1f930f67bb0b7e5cf2f77ba265b14b4e8bc3
Utsav Tamrakar <utsavtamrakar54321@gmail.com>
2025-10-06 23:25:33 +0545

    add

---
+++ javascript/JS part-1/script.js
@@ -0,0 +1,48 @@
+let js = "amazing";
+if (js === "amazing") alert("Javascript is fun");
+console.log(40 + 8 + 23 - 10);
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+let UtsavPrasisma = "Lover";
+console.log(UtsavPrasisma);
+// /Some errors due to keywords and while declaring variables/
+// let new = 27;
+// let function = 27;
+// let 3years = 27;
+// let jonas&matilda = "JM";
+// let let = 27;
+// If we include $ infront of keywords then it will work
+let $function = 27;
+let _3years = 3;
+let $new = 27;
+
+let person = "Jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+// This is single line comment
+/* This is
+multi line comment */
+
+let javascripIsFun = true;
+console.log(javascripIsFun);
+console.log(typeof true);
+console.log(typeof javascripIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+// We can use double quote or single quote while declaring the string but we can't use " this' while declaring the variable
+
+//Dynamic typing
+javascripIsFun = "YES!";
+console.log(typeof javascripIsFun);
